e1c868d67b9871d04f1dda7d4bbde7030ee9b799
skitheom <SahoKitahara@my.uopeople.edu>
2025-01-30 19:27:55 +0900

    .

--- 334.increasing-triplet-subsequence.cpp
+++ 334.increasing-triplet-subsequence.cpp
@@ -7,9 +7,26 @@
 // @lc code=start
 class Solution {
 public:
-    bool increasingTriplet(vector<int>& nums) {
-        
+  bool increasingTriplet(vector<int> &nums) {
+
+    int n = nums.size();
+
+    if (n < 3)
+      return false;
+
+    int low = std::numeric_limits<int>::max();
+    int mid = std::numeric_limits<int>::max();
+
+    for (size_t i = 0; i < n; ++i) {
+      if (nums[i] > mid)
+        return true;
+      else if (nums[i] < low) {
+        low = nums[i];
+      } else if (nums[i] > low && nums[i] < mid) {
+        mid = nums[i];
+      }
     }
+    return false;
+  }
 };
 // @lc code=end
-
