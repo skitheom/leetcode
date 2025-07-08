00e0e7d5e283f6e2689c4d46b1d4bcf5005d0c8e
skitheom <SahoKitahara@my.uopeople.edu>
2025-07-08 23:16:43 +0900

    expand palindrome

--- 5.longest-palindromic-substring.ts
+++ 5.longest-palindromic-substring.ts
@@ -8,60 +8,23 @@
 function longestPalindrome(s: string): string {
   if (s.length < 2) return s;
 
-  const isPalindromic = (s: string): boolean => {
-    let l = 0, r = s.length - 1;
-    while (l < r) {
-      if (s[l] !== s[r]) return false;
-      l++;
-      r--;
-    }
-    return true;
-  };
-
-  let result: string = s.slice(0, 1);
-  for (let start = 0; start < s.length; ++start) {
-    const ch = s[start];
-    let idx = s.lastIndexOf(ch);
-
-    while (start < idx) {
-      const tmpSubstr = s.slice(start, idx + 1);
-      if (isPalindromic(tmpSubstr)) {
-        result = (result.length < tmpSubstr.length) ? tmpSubstr : result;
-        break;
-      }
-      idx = s.lastIndexOf(ch, idx - 1);
-    }
-  }
-
-  return result;
-};
-// @lc code=end
-
-
-/*
-function longestPalindrome(s: string): string {
-  if (s.length < 2) return s;
-
   let start = 0;
   let end = 0;
 
-  const expand = (left: number, right: number): void => {
-    while (left >= 0 && right < s.length && s[left] === s[right]) {
-      left--;
-      right++;
+  const expandPalindrome = (l: number, r: number): void => {
+    while (l >= 0 && r < s.length && s[l] === s[r]) {
+      l--;
+      r++;
     }
-    // 終了時には1つ余計に広がってるので -1 する
-    if (right - left - 1 > end - start) {
-      start = left + 1;
-      end = right - 1;
+    if (r - l - 1 > end - start) {
+      start = l + 1;
+      end = r - 1;
     }
   };
-
   for (let i = 0; i < s.length; i++) {
-    expand(i, i);     // 奇数長中心
-    expand(i, i + 1); // 偶数長中心
+    expandPalindrome(i, i);
+    expandPalindrome(i, i + 1);
   }
-
   return s.slice(start, end + 1);
-}
-*/
+};
+// @lc code=end
